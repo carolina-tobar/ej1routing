9aa498f727b7d7387c4e91d57344cf82bc4b31cb
carolina <arlinb@gmail.com>
2020-07-31 14:11:48 -0300

    primer commit

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>
+        Nuestra misiôn es ofrecer un producto libre de componentes dañinos y
+        enaltecer la naturaleza.
+      </h2>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <ul>
+        <li>Masaje con piedras calientes del Tibet</li>
+        <li>Mascarilla de barro</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
